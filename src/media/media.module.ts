4c82775a4004227a3b0e933b3ac5c9af2e746a13
Tertha Dev Sarker <devteertha28@gmail.com>
2023-09-07 11:37:34 +0600

    Remove multer

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MulterModule } from '@nestjs/platform-express';
 
 import { MediaService } from './media.service';
 import { UtilService } from '../shared/services/util.service';
@@ -8,12 +7,7 @@ import { UtilService } from '../shared/services/util.service';
 import { MediaController } from './media.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MulterModule.register({
-      dest: './uploads', // Temporarily store uploaded files
-    }),
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [MediaController],
   providers: [MediaService, UtilService],
   exports: [MediaService],
